7345e9726afe4b71c828b940eac8e2f83921e930
gabriel15asouza <gabalves.souza@gmail.com>
2024-04-09 21:30:34 -0300

    atualizacao

--- JavaRead/src/CSVReader.java
+++ JavaRead/src/CSVReader.java
@@ -16,15 +16,21 @@ public class CSVReader {
 
 
             while((line = br.readLine()) != null ) {
-                String[] values = line.split(";");
-                System.out.println("Nome: "+ values[0]+", Idade: "+ values[1]);
-                Person person  = new Person(values[0], values[1]);
-                csv.adicionarElemento(person);
+                if (i!=0) {
+                    String[] values = line.split(";");
+                    System.out.println("Nome: " + values[0] + ", Idade: " + values[1]);
+                    Person person = new Person(values[0], values[1]);
+                    csv.adicionarElemento(person);
+                    i++;
+                } else {
+                    String[] indice = line.split(";");
+                    i++;
+                }
             }
             br.close();
-            for (i=0; i<people.size(); i++) {
-                System.out.println(people.get(i).getName());
-            }
+//            for (i=0; i<people.size(); i++) {
+//                System.out.println(people.get(i).getName());
+//            }
 
 
 
